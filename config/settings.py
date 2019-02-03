5047e026381b32e4ddca9470b8101a27597b0098
davlum <david.lum91@gmail.com>
2019-02-02 19:15:17 -0500

    rebase

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.environ['STATIC_ROOT']
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '')
 STATIC_URL = os.environ['STATIC_URL']
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
