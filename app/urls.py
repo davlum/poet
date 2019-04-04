cfc881d81637a28099707787a811fa055bf6b6db
davlum <david.lum@ssense.com>
2019-04-03 23:23:38 -0400

    Fixes #21 (#24)
    * Fixes #21

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import views as auth_views
 from app import views
+
 
 # https://docs.djangoproject.com/en/2.1/topics/i18n/translation/#translating-url-patterns
 urlpatterns = [
@@ -12,6 +14,12 @@ urlpatterns = [
     path(_('collection/<int:collection_id>'), views.collection, name='collection'),
     path(_('entity/<int:entity_id>'), views.entity, name='entity'),
     path(_('search/'), views.search, name='search'),
+    path(_('accounts/login/'), auth_views.LoginView.as_view(), name='login'),
+    path(_('logout/'), auth_views.LogoutView.as_view(), name='logout'),
+    path(_('password-reset/'), auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+    path(_('password-reset-done/'), auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path(_('reset/<uidb64>/<token>/'), auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path(_('password-complete/'), auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,6 @@ LOGGING = {
     },
 }
 
-FILE_UPLOAD_PERMISSIONS = os.environ['FILE_UPLOAD_PERMISSIONS']
-
 TEMPLATES = [
     {
         "BACKEND": "django_jinja.backend.Jinja2",
@@ -192,14 +190,18 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.environ['MEDIA_ROOT']
 MEDIA_URL = '/media/'
 
+
+FILE_UPLOAD_PERMISSIONS = 0o774
+
+
+# Email
 EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ['APP_MAIL_USERNAME']
 EMAIL_HOST_USER = 'poetica.sonora.auto@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ['APP_MAIL_PASSWORD']
 EMAIL_PORT = 587
 
-# Should be commented out for production
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = os.environ['EMAIL_BACKEND']
 
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, "locale"),
