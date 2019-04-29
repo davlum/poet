15fd9d6cf58f3d7c1a298173a5cad7e2b642a642
davlum <david.lum91@gmail.com>
2019-04-28 20:17:10 -0400

    Fixes #31

--- static/js/init_wavesurfer.js
+++ static/js/init_wavesurfer.js
@@ -3,7 +3,7 @@ function createWaveSurferElement(containerId, audioFilepath, waveformPeaks) {
     const peaksId = containerId + "-peaks";
     const timeRemainingId = containerId + "-time-remaining";
     const timeCurrentId = containerId + "-time-current";
-    const timeTotalId = containerId + "-time-total";
+    // const timeTotalId = containerId + "-time-total";
 
 
     const rootElement = document.getElementById(containerId);
@@ -53,7 +53,7 @@ function createWaveSurferElement(containerId, audioFilepath, waveformPeaks) {
     const volumeDiv = document.createElement("div");
     buttonsDiv.appendChild(volumeDiv);
 
-    volumeDiv.classList.add('col-md-6');
+    volumeDiv.classList.add('col-md-5');
 
     const volumeSlider = document.createElement('input');
     volumeDiv.appendChild(volumeSlider);
@@ -64,24 +64,24 @@ function createWaveSurferElement(containerId, audioFilepath, waveformPeaks) {
 
     const timeDiv = document.createElement('div'),
         remainingSpan = document.createElement('span'),
-        currentSpan = document.createElement('span'),
-        totalSpan = document.createElement('span');
+        currentSpan = document.createElement('span');
+        // totalSpan = document.createElement('span');
 
-    timeDiv.classList.add('col-md-4');
+    timeDiv.classList.add('col-md-5');
     timeDiv.classList.add('text-left');
     const spanClass = 'time-span';
-    totalSpan.classList.add(spanClass);
+    // totalSpan.classList.add(spanClass);
     currentSpan.classList.add(spanClass);
     remainingSpan.classList.add(spanClass);
 
     remainingSpan.id = timeRemainingId;
     currentSpan.id = timeCurrentId;
-    totalSpan.id = timeTotalId;
+    // totalSpan.id = timeTotalId;
 
     buttonsDiv.appendChild(timeDiv);
-    timeDiv.appendChild(remainingSpan);
     timeDiv.appendChild(currentSpan);
-    timeDiv.appendChild(totalSpan);
+    timeDiv.appendChild(remainingSpan);
+    // timeDiv.appendChild(totalSpan);
 
 
     wavesurfer.on('ready', function () {
@@ -94,9 +94,11 @@ function createWaveSurferElement(containerId, audioFilepath, waveformPeaks) {
         const currentTime = Math.round(wavesurfer.getCurrentTime());
         const remainingTime = Math.round(totalTime - currentTime);
 
-        document.getElementById(timeCurrentId).innerText = currentTime;
-        document.getElementById(timeTotalId).innerText = Math.round(totalTime);
-        document.getElementById(timeRemainingId).innerText = remainingTime;
+
+
+        document.getElementById(timeCurrentId).innerText = secondsToDisplayTime(currentTime);
+        // document.getElementById(timeTotalId).innerText = secondsToDisplayTime(Math.round(totalTime));
+        document.getElementById(timeRemainingId).innerText = secondsToDisplayTime(remainingTime);
 
         const onChangeVolume = function (e) {
             wavesurfer.setVolume(e.target.value);
@@ -114,9 +116,9 @@ function createWaveSurferElement(containerId, audioFilepath, waveformPeaks) {
             const remainingTime = Math.round(totalTime - currentTime);
 
 
-            document.getElementById(timeCurrentId).innerText = currentTime;
-            document.getElementById(timeTotalId).innerText = Math.round(totalTime);
-            document.getElementById(timeRemainingId).innerText = remainingTime;
+            document.getElementById(timeCurrentId).innerText = secondsToDisplayTime(currentTime);
+            // document.getElementById(timeTotalId).innerText = secondsToDisplayTime(Math.round(totalTime));
+            document.getElementById(timeRemainingId).innerText = secondsToDisplayTime(remainingTime);
         }
     });
 }
@@ -130,6 +132,16 @@ if (typeof audioIterator !== 'undefined') {
     }
 }
 
+function secondsToDisplayTime(seconds) {
+    const hours   = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    const secs = seconds - (hours * 3600) - (minutes * 60);
 
+    const strHrs = hours < 10 ? "0" + hours : hours;
+    const strMins = minutes < 10 ? "0" + minutes : minutes;
+    const strSecs = secs < 10 ? "0" + secs : secs;
+
+    return strHrs + ':' + strMins + ':' + strSecs;
+}
 
 
