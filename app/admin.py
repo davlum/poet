41719b69b3b34ff8a9e16ba502ac00b12220ba45
davlum <david.lum91@gmail.com>
2019-05-26 13:56:58 -0400

    Add search bars to admin, Fixes #40

--- app/admin.py
+++ app/admin.py
@@ -3,11 +3,51 @@ from simple_history.admin import SimpleHistoryAdmin
 from app.models.work import Work, WorkCollection
 from app.models.entity import Entity, EntityToEntityRel
 from app.models.relations import EntityToWorkRel
-# Register your models here.
 
 
-admin.site.register(Entity, SimpleHistoryAdmin)
-admin.site.register(EntityToEntityRel, SimpleHistoryAdmin)
-admin.site.register(Work, SimpleHistoryAdmin)
-admin.site.register(WorkCollection, SimpleHistoryAdmin)
-admin.site.register(EntityToWorkRel, SimpleHistoryAdmin)
+class EntityAdmin(SimpleHistoryAdmin):
+    list_display = ['__str__', 'release_state']
+    search_fields = ['full_name', 'alt_name']
+    ordering = ['release_state']
+
+
+class WorkAdmin(SimpleHistoryAdmin):
+    list_display = ['__str__', 'release_state']
+    search_fields = ['full_name', 'alt_name']
+    ordering = ['release_state']
+
+
+class EntityToEntityRelAdmin(SimpleHistoryAdmin):
+    list_display = ['__str__', 'relationship']
+    search_fields = [
+        'from_entity__full_name',
+        'from_entity__alt_name',
+        'to_entity__full_name',
+        'to_entity__alt_name',
+        'relationship'
+    ]
+    ordering = ['relationship']
+
+
+class EntityToWorkRelAdmin(SimpleHistoryAdmin):
+    list_display = ['__str__', 'relationship']
+    search_fields = [
+        'from_entity__full_name',
+        'from_entity__alt_name',
+        'to_work__full_name',
+        'to_work__alt_name'
+    ]
+    ordering = ['relationship']
+
+
+class WorkCollectionAdmin(SimpleHistoryAdmin):
+    list_display = ['__str__', 'release_state']
+    search_fields = ['collection_name']
+    ordering = ['release_state']
+
+
+admin.site.register(Entity, EntityAdmin)
+admin.site.register(EntityToEntityRel, EntityToEntityRelAdmin)
+admin.site.register(Work, WorkAdmin)
+admin.site.register(WorkCollection, WorkCollectionAdmin)
+admin.site.register(EntityToWorkRel, EntityToWorkRelAdmin)
