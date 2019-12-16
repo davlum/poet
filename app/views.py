f770a5238a67b06e6a546b4987d27269f2b6dc72
David Lum <david.lum91@gmail.com>
2019-12-16 13:44:39 -0500

    Ridirect '/' to search, fixes #50

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from app.controllers.work import get_work_context
 from app.controllers.search import get_search_context
@@ -26,7 +26,7 @@ def paginate_list(request, page_list, number_of_pages=10):
 
 
 def index(request):
-    return render(request, 'poet/index.html.j2')
+    return redirect('/inicio')
 
 
 def home(request):
